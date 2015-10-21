e12fda4c949a981154502b540a9eff7894bd2a08
codeamac07 <codeamac07@gmail.com>
2015-10-21 12:10:55 -0500

    crea la interfaz

--- class _Tienda.rb
+++ class _Tienda.rb
@@ -1,2 +1,31 @@
 class Tienda
+  def initialize(nombre)
+    @nombre = nombre
+  end
+
+  def interfaz
+    puts "Welcome to the online store: #{@nombre}" 
+    puts "__________________________________________________"
+    puts "Select option number"
+    puts "1. Login"
+    puts "2. Register"
+    puts "3. Exit"
+    choice = gets.chomp
+    case choice
+    when "1"
+      login
+    when "2"
+      register
+    when "3"
+      puts "Gracias por visitarnos"
+    end
+
+  end
+
+  def login
+  end
+
+  def register
+  end
+
 end
